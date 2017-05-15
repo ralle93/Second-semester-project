c968b04e3bfde2d2c3ec066628a0db1df2a75fbe
Rasmus Thrane <snubi68@gmail.com>
2017-05-15 11:03:14 +0200

    mail class

--- src/Mail.java
+++ src/Mail.java
@@ -1,4 +1,10 @@
+import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 /**
@@ -11,24 +17,56 @@ public class Mail {
    private String subject;
    Properties props;
    Session sesh;
+   private String host = "localhost";
 
+   public Mail(){}
 
    public Mail(String to, String from, String content, String subject){
       this.to = to;
       this.from = from;
       this.content = content;
       this.subject = subject;
-      props = new Properties();
+      props = System.getProperties();
+      props.setProperty("mail.smtp.host", host);
       sesh =  Session.getDefaultInstance(props);
    }
 
-   public void mailToCustomer(){
 
-   }
 
 
    public static void main(String[] args) {
 
    }
 
+   public String getTo() {
+      return to;
+   }
+
+   public void setTo(String to) {
+      this.to = to;
+   }
+
+   public String getFrom() {
+      return from;
+   }
+
+   public void setFrom(String from) {
+      this.from = from;
+   }
+
+   public String getContent() {
+      return content;
+   }
+
+   public void setContent(String content) {
+      this.content = content;
+   }
+
+   public String getSubject() {
+      return subject;
+   }
+
+   public void setSubject(String subject) {
+      this.subject = subject;
+   }
 }
