c251468d8e92b866845a7cfa0e312426ef603cea
Søren Skovgaard <soren.ds@gmail.com>
2017-05-15 14:15:50 +0200

    Package names updated.

--- src/datalayer/User.java
+++ src/datalayer/User.java
@@ -4,12 +4,14 @@ public class User {
    private String email;
    private String password;
    private String name;
+   private String phoneNumber;
    private boolean admin;
 
-   public User(String email, String password, String name) {
+   public User(String email, String password, String name, String phoneNumber) {
       this.email = email;
       this.password = password;
       this.name = name;
+      this.phoneNumber;
    }
 
    public String getEmail() {
