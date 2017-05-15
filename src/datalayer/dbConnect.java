8d73121dd66ed59e76dc7d9ea8557f4b53fa0ba1
Rasmus Thrane <snubi68@gmail.com>
2017-05-15 13:02:23 +0200

    mail class

---
+++ src/datalayer/dbConnect.java
@@ -0,0 +1,38 @@
+package datalayer;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public final class dbConnect {
+
+   public Connection con;
+   public Statement stmt;
+   public static dbConnect db;
+   private dbConnect() {
+      String DBURL = "jdbc:mysql://mydbinstance.c71ud1joo1ez.eu-central-1.rds.amazonaws.com:3306/mydb?useSSL=false";
+      String driver = "com.mysql.jdbc.Driver";
+      String usern = "SSP";
+      String pass = "123456";
+      try {
+         Class.forName(driver).newInstance();
+         this.con = DriverManager.getConnection(DBURL,usern,pass);
+      } catch (Exception ex) {
+         ex.printStackTrace();
+         System.out.println(ex);
+      }
+
+   }
+
+   public static synchronized dbConnect getConnection(){
+      if(db == null){
+         db = new dbConnect();
+      }
+      return db;
+   }
+
+   public static void main(String[] args)throws SQLException {
+      Connection l = dbConnect.getConnection().con;
+      System.out.println(l.toString());
+   }
+}
