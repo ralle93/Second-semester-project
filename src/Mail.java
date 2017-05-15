aaa5f87d04e07b875912c8c81b3918286e8c2643
Rasmus Thrane <snubi68@gmail.com>
2017-05-15 10:02:28 +0200

    mail class

--- src/Mail.java
+++ src/Mail.java
@@ -1,5 +1,34 @@
+import javax.mail.Session;
+import java.util.Properties;
+
 /**
  * Created by rasmusthrane on 15/05/17.
  */
 public class Mail {
+   private String to;
+   private String from;
+   private String content;
+   private String subject;
+   Properties props;
+   Session sesh;
+
+
+   public Mail(String to, String from, String content, String subject){
+      this.to = to;
+      this.from = from;
+      this.content = content;
+      this.subject = subject;
+      props = new Properties();
+      sesh =  Session.getDefaultInstance(props);
+   }
+
+   public void mailToCustomer(){
+
+   }
+
+
+   public static void main(String[] args) {
+
+   }
+
 }
