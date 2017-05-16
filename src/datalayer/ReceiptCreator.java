dfa375490d42492016f530c511192db0d69cd082
Mikkel Olsen <Mikk7506@stud.kea.dk>
2017-05-16 12:55:00 +0200

    PDF Creation Second Draft

--- src/datalayer/ReceiptCreator.java
+++ src/datalayer/ReceiptCreator.java
@@ -1,11 +1,25 @@
 package datalayer;
 
-      import org.apache.pdfbox.pdmodel.PDDocument;
-      import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDDocumentInformation;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
-      import java.io.IOException;
+import java.io.IOException;
+import java.util.Calendar;
 
 public class ReceiptCreator {
+   private static int receiptID = 1;
+
+   private static String author = "Pernille Jensen";
+   private static String title = "Kage Kvittering";
+   private static String creator = "Pernilles Kager";
+   private static String subject = "Kage Bestilling";
+
+   /** DIMENSIONS OF PDF DOCUMENTS
+    * IN PIXELS: 792 height / 612 width
+    **/
 
    // TIL tests af pdfBox funktioner
    public static void main(String[] args) throws IOException {
@@ -14,9 +28,32 @@ public class ReceiptCreator {
       PDPage blankpage = new PDPage();
       document.addPage(blankpage);
 
-      document.save("pdf_examples/my_doc.pdf");
+      PDImageXObject pdImage = PDImageXObject.createFromFile("src/datalayer/ReceiptData/Logo.png", document);
+      PDPageContentStream contents = new PDPageContentStream(document, blankpage);
+      contents.drawImage(pdImage,0,692,240,100);
+      contents.close();
+
+      document.setDocumentInformation(addInformation(document));
+      document.save("pdf_examples/KageKvittering" + appendZeros(receiptID) + ".pdf");
       System.out.println("PDF created!");
 
       document.close();
+   }
+
+   private static String appendZeros(int number) {
+      return String.format("%06d", number);
+   }
+
+   private static PDDocumentInformation addInformation(PDDocument document) {
+      PDDocumentInformation pdd = document.getDocumentInformation();
+
+      pdd.setAuthor(author);
+      pdd.setTitle(title);
+      pdd.setCreator(creator);
+      pdd.setSubject(subject);
+      Calendar date = Calendar.getInstance();
+      pdd.setCreationDate(date);
+
+      return pdd;
    }
 }
