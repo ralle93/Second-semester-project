53f6923241f1a2584eef30fc4f2e39b3bcc2dd7c
Rasmus Thrane <snubi68@gmail.com>
2017-05-19 10:32:50 +0200

    gmail api added

--- src/applayer/SendGmail.java
+++ src/applayer/SendGmail.java
@@ -1,51 +1,29 @@
 
 package applayer;
 import com.sun.mail.smtp.SMTPTransport;
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+import java.io.IOException;
 import java.security.Security;
 import java.util.Date;
 import java.util.Properties;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.mail.*;
+import javax.mail.internet.*;
 
 public class SendGmail {
 
 
-   private SendGmail() {
-   }
-
    public static void main(String[] args) throws MessagingException{
-      Send("snubi68", "", "rasmusdreierthrane@gmail.com", "tester","this is a test");
+      //SendToCustomer("pernilleslaekkerier", "Abcdefg123456789", "soren.ds@gmail.com", "tester","this is a test");
+      sendWithAttach(  "mikk7506@stud.kea.dk","PDF INVOICE","there is a pdf", "pdf_examples/KageKvittering000001.pdf");
    }
 
-   public static void Send(final String username, final String password, String recipientEmail, String title, String message) throws AddressException, MessagingException {
-      SendGmail.Send(username, password, recipientEmail, "", title, message);
-   }
-
-   public static void SendWithAttach(final String username, final String password, String recipientEmail, String title, String message) throws AddressException, MessagingException{
-
-   }
-
-   public static void Send(final String username, final String password, String recipientEmail, String ccEmail, String title, String message) throws AddressException, MessagingException {
+   public static void sendToCustomer(final String username, final String password, String recipientEmail, String title, String message) throws AddressException, MessagingException {
       Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
-      final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
-
-      // Get a Properties object
-      Properties props = System.getProperties();
-      props.setProperty("mail.smtps.host", "smtp.gmail.com");
-      props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
-      props.setProperty("mail.smtp.socketFactory.fallback", "false");
-      props.setProperty("mail.smtp.port", "465");
-      props.setProperty("mail.smtp.socketFactory.port", "465");
-      props.setProperty("mail.smtps.auth", "true");
-
-
-      props.put("mail.smtps.quitwait", "false");
-
-      Session session = Session.getInstance(props, null);
+      Session session = Session.getInstance(getProperties(), null);
 
       // -- Create a new message --
       final MimeMessage msg = new MimeMessage(session);
@@ -54,18 +32,61 @@ public class SendGmail {
       msg.setFrom(new InternetAddress(username + "@gmail.com"));
       msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail, false));
 
-      if (ccEmail.length() > 0) {
-         msg.setRecipients(Message.RecipientType.CC, InternetAddress.parse(ccEmail, false));
-      }
-
       msg.setSubject(title);
       msg.setText(message, "utf-8");
       msg.setSentDate(new Date());
 
-      SMTPTransport t = (SMTPTransport)session.getTransport("smtps");
+      SMTPTransport t = (SMTPTransport) session.getTransport("smtps");
 
       t.connect("smtp.gmail.com", username, password);
       t.sendMessage(msg, msg.getAllRecipients());
       t.close();
+
+   }
+
+   public static void sendWithAttach( String recipientEmail, String title, String message, String pdfFilePath) throws AddressException, MessagingException{
+      final String username = "pernilleslaekkerier@gmail.com";
+      final String password = "Abcdefg123456789";
+      Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+      Session session = Session.getInstance(getProperties(), null);
+
+      //create msg, msgBody object and set subject, recipient
+      Message msg = new MimeMessage(session);
+      msg.setFrom(new InternetAddress("pernilleslaekkerier@gmail.com"));
+      msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipientEmail, false));
+      msg.setSubject(title);
+      BodyPart msgBody = new MimeBodyPart();
+      msgBody.setText(message);
+
+      Multipart multipart = new MimeMultipart();
+      multipart.addBodyPart(msgBody);
+
+      // create the attachment and set it to the email
+      msgBody = new MimeBodyPart();
+      DataSource src = new FileDataSource(pdfFilePath);
+      msgBody.setDataHandler(new DataHandler(src));
+      msgBody.setFileName(pdfFilePath);
+      multipart.addBodyPart(msgBody);
+      msg.setContent(multipart);
+
+      SMTPTransport t = (SMTPTransport) session.getTransport("smtps");
+
+      t.connect("smtp.gmail.com", username, password);
+      t.sendMessage(msg, msg.getAllRecipients());
+      t.close();
+   }
+
+   public static Properties getProperties(){
+      Properties props = System.getProperties();
+      props.setProperty("mail.smtps.host", "smtp.gmail.com");
+      props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+      props.setProperty("mail.smtp.socketFactory.fallback", "false");
+      props.setProperty("mail.smtp.port", "465");
+      props.setProperty("mail.smtp.socketFactory.port", "465");
+      props.setProperty("mail.smtps.auth", "true");
+
+      props.put("mail.smtps.quitwait", "false");
+
+      return props;
    }
 }
