9693b8ef40a165495cf48f174fb20cd17e245683
Tommy Troest <tommytroest@Tommys-MacBook-Pro-2.local>
2017-05-21 17:52:02 +0200

    Added a Cart Class

--- src/applayer/Cart.java
+++ src/applayer/Cart.java
@@ -10,27 +10,89 @@ import java.util.HashMap;
 public class Cart {
 
    private LineItem[] list = new LineItem[RecInfo.getMaxOrderSize()];
-   private int totalPrice;
+   private int totalPrice = 0;
 
-
-   public Cart(int totalPrice) {
+   // Constructor for Cart
+   public Cart() {
       this.totalPrice = totalPrice;
    }
 
-
-   public void addItem() {
-
+   // Add Items method to Cart
+   private void addItem(LineItem lineItem) {
+      for (int i = 0; i <= list.length; i++) {
+         if (list[i] == null) {
+            list[i] = lineItem;
+            totalPrice += lineItem.getPrice();
+            return;
+         }
+      }
+      System.out.println("Cannot add any more items to basket.");
    }
 
-   public void deleteItem() {
-
+   // Delete Existing item from Cart.
+   private void deleteItem(int indexLine) {
+      for (int i = 0; i < list.length; i++) {
+         if (list[i] != null && i == indexLine) {
+            totalPrice -= list[i].getPrice();
+            list[i] = null;
+            rearrangeList();
+            return;
+         }
+      }
+      System.out.println("No such item in the cart");
    }
 
-   public void clearCart() {
-
+   // Rearrange Method for Cart.
+   private void rearrangeList() {
+      for (int i = 0; i < list.length; i++) {
+         if (list[i] == null) {
+            if ( list[i] != null && i < list.length) {
+               list[i] = list[i + 1];
+               list[i + 1] = null;
+            }
+         }
+      }
    }
 
-   public LineItem[] getList() {
+   // ClearCart Method.
+   private void clearCart() {
+      for (int i = 0; i < list.length; i++) {
+         list[i] = null;
+      }
+   }
+
+   // getList Method for Cart.
+   private LineItem[] getList() {
+      for (LineItem aList : list) {
+         if (aList != null) {
+            System.out.println(aList.getCake());
+         }
+      }
+      System.out.println("End of the list.");
       return list;
    }
+
+   // TESTING.
+   public static void main(String[] args) {
+      Cake testCake1 = new Cake(1,250,"TestKage","Den smager af fisk.");
+      Cake testCake2 = new Cake(2,250,"TestCake2","Den smager også af fisk");
+
+      LineItem testItem1 = new LineItem(testCake1, 2, "none");
+      LineItem testItem2 = new LineItem(testCake2, 1,"Fisk");
+
+      Cart testCart = new Cart();
+      System.out.println("ADD");
+      testCart.addItem(testItem1);
+      testCart.addItem(testItem2);
+      System.out.println("GET LIST");
+      System.out.println(testCart.getList());
+      System.out.println("DELETE ITEM");
+      testCart.deleteItem(0);
+      System.out.println("GET LIST");
+      System.out.println(testCart.getList());
+      System.out.println("CLEAR");
+      testCart.clearCart();
+      System.out.println("Current Cart: " + testCart.getList());
+   }
+
 }
