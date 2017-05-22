3f4f27c724e13f987cf442d4e7af98251ea0a2b0
Rasmus Thrane <snubi68@gmail.com>
2017-05-22 13:39:50 +0200

    Security package created

--- src/Security/Hash.java
+++ src/Security/Hash.java
@@ -3,11 +3,10 @@ package Security;
 public class Hash {
 
    public static String hashPW(String str){
-
-      return str.hashCode() +"";
+      return str.hashCode() + "";
    }
 
    public static void main(String[] args) {
-      System.out.println(hashPW("1234"));
+      System.out.println(hashPW("test123"));
    }
 }
