bd66c284a5e72e6781e16b561932edfa6b5c142a
sorends <soren.ds@gmail.com>
2017-05-23 16:42:42 +0200

    Order system implemented. Missing user confirm order page and add the order to database.

--- src/servlets/ShoppingCart.java
+++ src/servlets/ShoppingCart.java
@@ -14,9 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 
-/**
- * Created by tommytroest on 21/05/2017.
- */
 @WebServlet(name = "ShoppingCart")
 public class ShoppingCart extends HttpServlet {
 
